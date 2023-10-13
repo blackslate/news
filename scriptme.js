9af3d465bb5e4c2c244940ea936e3f2dc6d7a69b
James Newton <blackslate@lexogram.com>
2023-10-13 12:22:17 +0300

    Refactor code
    * Add treatArticle function
    * Add

--- scriptme.js
+++ scriptme.js
@@ -27,64 +27,65 @@ function fetchHeadlines() {
         alert('An error occurred while fetching headlines. Please try again later.');
     });
 }
+
+
+const treatSave = (article, articleDiv) => {
+    // Get the article's HTML content
+    const articleHTML = articleDiv.textContent;
+    console.log('Garen1', articleHTML);
+    
+    // Store the article in local storage
+    localStorage.setItem('savedArticle',JSON.stringify(article));
+
+    // Provide feedback to the user (optional)
+    alert('Article saved to local storage!');
+    const savedArticle = JSON.parse(localStorage.getItem('savedArticle')) ;
+    console.log('********************', savedArticle);
+    
+    const savedList = document.createElement('li');
+    savedList.classList.add('liSaved')
+
+    const savedListItem = document.createElement('li');
+    savedListItem.classList.add('liSaved');
+
+     savedList.innerHTML =  articleDiv.innerHTML //`<h3>${article.title}</h3><img src = '${article.urlToImage}'/>` ;
+     ulSavedList.appendChild(savedList);
+}
+
+
+const treatArticle = article => {
+    const articleDiv = document.createElement('div')
+    articleDiv.classList.add('article') ;
+    const title = document.createElement('h3');
+    title.textContent = article.title; // adding articles to the h3
+    const description = document.createElement('p');
+    description.textContent = article.description;
+    const link = document.createElement('a');
+    link.textContent = 'Read more'; 
+    link.href = article.url;
+    link.target = '_blank';
+    const imageElement = document.createElement('img');
+    imageElement.src = article.urlToImage; // Replace with the actual image path
+    imageElement.alt = 'Image Description'; // Replace with a description for the image
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'Save';
+    saveButton.classList.add('saveButton');
+    // Add a click event listener to the save button
+    saveButton.addEventListener('click', () => treatSave(article, articleDiv));
+   
+
+    articleDiv.appendChild(title);
+    articleDiv.appendChild(description);
+    articleDiv.appendChild(link);
+    articleDiv.appendChild(saveButton);
+    articleDiv.appendChild(imageElement);
+    divNewsList.appendChild(articleDiv);
+}
+
+
 function displayNews(articles) {
      divNewsList.innerHTML = ''; 
-     articles.forEach(article => {
-        const articleDiv = document.createElement('div')
-        articleDiv.classList.add('article') ;
-        const title = document.createElement('h3');
-        title.textContent = article.title; // adding articles to the h3
-        const description = document.createElement('p');
-        description.textContent = article.description;
-        const link = document.createElement('a');
-        link.textContent = 'Read more'; 
-        link.href = article.url;
-        link.target = '_blank';
-        const imageElement = document.createElement('img');
-        imageElement.src = article.urlToImage; // Replace with the actual image path
-        imageElement.alt = 'Image Description'; // Replace with a description for the image
-        const saveButton = document.createElement('button');
-        saveButton.textContent = 'Save';
-        saveButton.classList.add('saveButton');
-        // Add a click event listener to the save button
-        saveButton.addEventListener('click', () => {
-            // Get the article's HTML content
-            const articleHTML = articleDiv.textContent;
-            console.log('Garen1', articleHTML);
-            
-            // Store the article in local storage
-            localStorage.setItem('savedArticle',JSON.stringify(article));
-
-            // Provide feedback to the user (optional)
-            alert('Article saved to local storage!');
-            const savedArticle = JSON.parse(localStorage.getItem('savedArticle')) ;
-            console.log('********************', savedArticle);
-            
-            const savedList = document.createElement('li');
-            savedList.classList.add('liSaved')
-
-            const savedListItem = document.createElement('li');
-            savedListItem.classList.add('liSaved');
-
-             savedList.innerHTML =  articleDiv.innerHTML //`<h3>${article.title}</h3><img src = '${article.urlToImage}'/>` ;
-             ulSavedList.appendChild(savedList);
-
-
-        });
-       
-
-        articleDiv.appendChild(title);
-        articleDiv.appendChild(description);
-        articleDiv.appendChild(link);
-        articleDiv.appendChild(saveButton);
-        articleDiv.appendChild(imageElement);
-        divNewsList.appendChild(articleDiv);
- 
-        
-    
-    
-    
-    });
+     articles.forEach(treatArticle);
    
 }
 
